04f1676fdda49334605390524a968b823369d43f
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-06-25 16:31:18 -0400

    Removed commented code from file
    There was some old code that I commented out at the bottomm of the file. Now that I have
    the gameLogic at a good stopping point I have removed the  comments.

--- assets/auth/gameLogic.js
+++ assets/auth/gameLogic.js
@@ -72,38 +72,6 @@ const checkForWin = function (playerMoves) {
 const ttt = function () {
 
 }
-//   for (let i = 0; i < gridContainer.length; i++) {
-//     if (i >= 0 && i <= 3 ) {
-//       row0.push(gridContainer[i])
-//       // ['d', 'i', 'b', 't']
-//       // on row 0
-//     } else if (i > 3 && i <= 7) {
-//       row1.push(gridContainer[i])
-//       // ['l', 'o', 'a', 'm']
-//       // on row 1
-//     } else{
-//       row2.push(gridContainer[i])
-//       // ['b', 'c','g', 'r']
-//       // on row2
-//     }
-
-//   }
-//   const rows = [row0, row1, row2, row3]
-
-//   const word = []
-
-//   for (let i = 0; i < coordinatePairs.length; i++) {
-//     const coordPair = coordinatePairs[i] // when i = 0, [0,0] when i = 1, [1,1]
-//     // get correct letter
-//     const letter = rows[coordPair[0]][coordPair[1]]
-//     word.push(letter)
-//   }
-
-//   console.log(word.join(''))
-//   // returns a string representing
-//   // the concat of the letters at each of the coordinates
-// }
-
 
 module.exports = {
   clickEvent
