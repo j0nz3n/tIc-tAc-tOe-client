89bc0cc8d55451382ab3c4f31a2592fecaa8e9e8
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-06-25 16:19:06 -0400

    Updated gameLogic
    Game is now abble to track the Player moves and for the time being will display an alert
    that says which player has won.

--- assets/auth/gameLogic.js
+++ assets/auth/gameLogic.js
@@ -2,47 +2,74 @@
 
 // funtion that takes a gridContainer and userClick
 // a gridContainer is the tic-tac-toe board
-// userClick is the child element that was clicked by the player
-// each sub-array is the letter we are looking at to determin a win, loss or draw
 
 const token = ['X', 'O']
+let gameOver = false
 let turn = true // true = X | false = O
 let xMoves = [] // array to hold player moves
 let oMoves = [] // will be used to check for win
 let thisWasClicked
-const row2 = []
-const row0 = []
-const row1 = []
 
+// Handles the clicked div and calls move according to who's
+// turn it is
 const clickEvent = function (event) {
   thisWasClicked = event.target.id
-  // console.log("you clicked: ", thingClicked)
   console.log("This Was Clicked: ", thisWasClicked)
   turn ? xTurn(thisWasClicked) : oTurn(thisWasClicked)
 }
 
 const xTurn = function (thisWasClicked) {
   xMoves.push(thisWasClicked)
-  document.getElementById(thisWasClicked).innerHTML = token[0]
-  turn = false
-  console.log("X has done: ", xMoves, turn)
-  thisWasClicked = ''
+  let thingClicked = document.getElementById(thisWasClicked)
+  thingClicked.innerHTML = token[0]
+  $(thingClicked).addClass('unclickable')
+  if (xMoves.length >= 3) {
+    checkForWin(xMoves)
+  } else {
+    turn ? turn = false : turn = true
+    thisWasClicked = ''
+  }
   return turn
 }
-
 
 const oTurn = function (thisWasClicked) {
   oMoves.push(thisWasClicked)
-  document.getElementById(thisWasClicked).innerHTML = token[1]
-  turn = true
-  console.log("O has done: ", oMoves, turn)
-  thisWasClicked = ''
+  let thingClicked = document.getElementById(thisWasClicked)
+  thingClicked.innerHTML = token[1]
+  $(thingClicked).addClass('unclickable')
+  if (oMoves.length >= 3) {
+    checkForWin(oMoves)
+  } else {
+    turn ? turn = false : turn = true
+    thisWasClicked = ''
+  }
   return turn
 }
 
-const ttt = function (gridContainer, coordinatePairs) {
-  // step 1 find letter for first coordinate
-
+const checkForWin = function (playerMoves) {
+  const winScenario = [
+    ["one", "two", "three"], // top row
+    ["four", "five", "six"], // middle row
+    ["seven", "eight", "nine"], // bottom row
+    ["one", "four", "seven"], // left column
+    ["two", "five", "eight"], // middle column
+    ["three", "six", "nine"], // right column
+    ["one", "five", "nine"], // left to right diagnal
+    ["three", "five", "seven"] // right to left diagnal
+  ]
+  winScenario.forEach(function (scenario) {
+      if (scenario.includes(playerMoves[0]) && scenario.includes(playerMoves[1]) && scenario.includes(playerMoves[2])) {
+        console.log("Winner!")
+        turn ? alert("X Wins!") : alert('O Wins')
+        gameOver = true
+      }
+  })
+    console.log("Player has done: ", playerMoves, turn)
+    turn ? turn = false : turn = true
+    thisWasClicked = ''
+    return turn, gameOver
+}
+const ttt = function () {
 
 }
 //   for (let i = 0; i < gridContainer.length; i++) {
